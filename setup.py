8dd8821c742573f8a3d5f57e9c69955b0aa47e1f
Alex Cannon <alex.cannon@moat.com>
2017-11-01 14:42:24 -0400

    Sync pkg version and git tag version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jenkins-backup-s3',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
