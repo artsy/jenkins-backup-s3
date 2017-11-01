ddc60679dad7a2bed018a99c43c2965f513ca85e
Alex Cannon <alex.cannon@moat.com>
2017-11-01 12:54:46 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jenkins-backup-s3',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
