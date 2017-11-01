20a509bad6f2e6bb2fd349f229a837a3280aa5f4
Alex Cannon <alex.cannon@moat.com>
2017-11-01 12:43:31 -0400

    Fix entry points, update .gitignore

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='jenkins-backup-s3',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'backup-jenkins=backup-jenkins.backup:main'
+            'backup-jenkins=backup:main'
         ]
     },
     install_requries=(
