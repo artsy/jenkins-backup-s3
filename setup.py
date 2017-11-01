97775a844e916434ad1dda39391a1b9815850bad
Alex Cannon <alex.cannon@moat.com>
2017-11-01 10:53:49 -0400

    Move backup.py into package, add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jenkins-backup-s3',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'backup-jenkins=backup-jenkins.backup:main'
+        ]
+    },
+    install_requries=(
+        'boto3==1.4.7'
+        'botocore==1.7.19',
+        'click==6.7',
+        'colorama==0.3.9',
+        'docutils==0.14',
+        'jmespath==0.9.3',
+        'python-dateutil==2.6.1',
+        's3transfer==0.1.11',
+        'six==1.11.0',
+        'termcolor==1.1.0',
+    )
+)
